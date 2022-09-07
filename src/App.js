e8ff957dcf7be006814a3511dfa13bb146098058
Ben Johnson <1989bjohnson@gmail.com>
2022-09-06 19:00:46 -0700

    added gallery route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
 import './App.scss';
 import { Slider } from './components/slider/Slider';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Gallery } from './components/gallery/Gallery';
 
 function App() {
   return (
     <>
-      <Slider />
+      <BrowserRouter>
+        <div className="nav-container">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Slider</Link>
+              </li>
+              <li>
+                <Link to="/gallery">Gallery</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="app-container">
+          <Routes>
+            <Route path="/" element={<Slider />} />
+            <Route path="/gallery" element={<Gallery />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/gallery/Gallery.jsx
@@ -0,0 +1,9 @@
+import "./Gallery.scss";
+
+export const Gallery = () => {
+  return (
+    <div className="gallery">
+      <h1>Gallery</h1>
+    </div>
+  );
+};
